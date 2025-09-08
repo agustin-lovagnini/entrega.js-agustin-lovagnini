53869687807be5e5be47129c9458815d70856184
Agustin <agustin.lovagnini.v@gmail.com>
2025-09-08 16:24:38 -0300

    arme la estructura de las inversiones y el prompt para pedir e verificar

---
+++ script.js
@@ -0,0 +1,19 @@
+const moneda = ["Pesos", "USDT", "USDS", "BTS", "Ethereum"];
+
+let inversion = [0, 0, 0, 0, 0];
+
+function pedirDeposito() {
+    let deposito = parseFloat(prompt("Ingrese el monto a depositar:"));
+
+    while (isNaN(deposito) || deposito <= 0) {
+        let mensaje;
+        if (isNaN(deposito)) {
+            mensaje = "No es un numero, ingresa un numero valido, podes sumar decimales";
+        } else {
+            mensaje = "El numero debe ser mayor a 0";
+        }
+        deposito = parseFloat(prompt(mensaje));
+    }
+    return deposito;
+}
+
